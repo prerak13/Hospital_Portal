e06b476c47c82c3783802c1ef9a23fbdb2f885e4
Prerak Choksi <prerakchoksi@Preraks-MacBook-Pro.local>
2022-03-29 16:15:31 -0300

    added changes in dashboard

--- Backend/controllers/userDashboardController.js
+++ Backend/controllers/userDashboardController.js
@@ -2,10 +2,10 @@ import AppointmentSchemaModel from "../models/appointmentBookingModel.js";
 import PModel from "../models/pathalogyModel.js";
 
 const getAppointments = async (req, res) => {
-  res.json(await AppointmentSchemaModel.find());
+  res.json(await AppointmentSchemaModel.find({ email: req.query.email }));
 };
 
 const getLapReports = async (req, res) => {
-  res.json(await PModel.find());
+  res.json(await PModel.find({ patientEmail: req.query.email }));
 };
 export { getAppointments, getLapReports };
