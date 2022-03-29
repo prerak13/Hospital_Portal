24ec008b5641817410d26bf392481bbf4806c0b4
Harivansh Bhatia <hr513288@dal.ca>
2022-03-29 11:32:06 -0300

    added author

--- Backend/controllers/docAppointmentController.js
+++ Backend/controllers/docAppointmentController.js
@@ -1,3 +1,8 @@
+/**
+ * @author Harivansh Bhatia
+ * @email hr513288@dal.ca
+ */
+
 import AppointmentSchemaModel from "../models/appointmentBookingModel.js";
 
 const bookAppointment = async (req, res) => {
--- Backend/models/appointmentBookingModel.js
+++ Backend/models/appointmentBookingModel.js
@@ -1,3 +1,8 @@
+/**
+ * @author Harivansh Bhatia
+ * @email hr513288@dal.ca
+ */
+
 import mongoose from "mongoose";
 
 const appointmentSchema = mongoose.Schema({
