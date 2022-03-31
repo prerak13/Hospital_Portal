e63e4e0185c5223f47aa32a68964e7b2ea344cfc
Harivansh Bhatia <hr513288@dal.ca>
2022-03-31 02:28:01 -0300

    test

--- Backend/server.js
+++ Backend/server.js
@@ -36,10 +36,10 @@ app.use("/api/pathology/", pathalologyRoutes);
 const __dirname = path.resolve();
 //deploying app
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "Frontend", "build")));
+  app.use(express.static(path.join(__dirname, "../Frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../Frontend/build/index.html"))
   );
 } else {
   app.get("/", (req, res) => {
