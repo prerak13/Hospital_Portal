491969ee0b74195586e6df6bcfe7f266f4fef203
@damania <fr454807@dal.ca>
2022-04-07 13:07:15 -0300

    added link to header for payment module

--- Frontend/src/Components/Header.js
+++ Frontend/src/Components/Header.js
@@ -28,7 +28,7 @@ function Header({ setSearch }) {
         <Navbar.Brand href="/">
           <h4 className="my-0">WeCare Solutions</h4>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="m-auto"></Nav>
 
@@ -72,6 +72,15 @@ function Header({ setSearch }) {
                   View Appointments
                 </NavDropdown.Item>
               </NavDropdown>
+            </Nav>
+          ) : (
+            ""
+          )}
+               {userInfo ? (
+            <Nav>
+             <Nav.Link href="/paymentform" activeStyle>
+              Pay Online
+            </Nav.Link>
             </Nav>
           ) : (
             ""
